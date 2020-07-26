2ed79918e4f054eb93219600368213e89974b6eb
ahenteti <ahmad.henteti@gmail.com>
2020-07-26 22:35:42 +0200

    add calender app header

---
+++ src/app/month/components/month-header/month-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-month-header',
+  templateUrl: 'month-header.component.html',
+  styleUrls: ['month-header.component.scss'],
+})
+export class MonthHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/month/month.module.ts
+++ src/app/month/month.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MonthDayComponent } from './components/month-day/month-day.component';
 import { MonthComponent } from './components/month/month.component';
+import { MonthHeaderComponent } from './components/month-header/month-header.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MonthDayComponent, MonthComponent],
+  declarations: [MonthDayComponent, MonthComponent, MonthHeaderComponent],
   providers: [],
   exports: [MonthComponent],
 })
