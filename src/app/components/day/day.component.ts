1e5a04813729e59fdd5eb0e49097474ea33e5266
ahenteti <ahmad.henteti@gmail.com>
2020-07-27 23:55:26 +0200

    display events

--- src/app/components/day/day.component.ts
+++ src/app/components/day/day.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, ElementRef, AfterViewChecked, HostListener, Output, EventEmitter } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewChecked, HostListener, Output, EventEmitter, OnInit } from '@angular/core';
 import { Renderer2 } from '@angular/core';
 import { Day } from '../../models/day.model';
+import { StorageService } from 'src/app/services/storage.service';
+import { Event } from 'src/app/models/event.model';
 
 @Component({
   selector: 'app-day',
   templateUrl: 'day.component.html',
   styleUrls: ['day.component.scss'],
 })
-export class DayComponent implements AfterViewChecked {
+export class DayComponent implements AfterViewChecked, OnInit {
   @Input()
   day: Day;
-
   @Output('createEvent')
   private _createEvent = new EventEmitter<Day>();
+  events: Event[];
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+  constructor(private renderer: Renderer2, private elementRef: ElementRef, private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    this.events = this.storageService.loadEvents(this.day);
+  }
 
   ngAfterViewChecked() {
     this.setElementHeight();
