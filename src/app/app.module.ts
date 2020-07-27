58922504b543e08a8cd821de7f3d0e65609ce11b
ahenteti <ahmad.henteti@gmail.com>
2020-07-27 19:55:14 +0200

    refactoring - add icon-button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { MonthComponent } from './components/month/month.component';
 import { DayComponent } from './components/day/day.component';
 import { HeaderComponent } from './components/header/header.component';
+import { IconButtonComponent } from './components/icon-button/icon-button.component';
 
 @NgModule({
-  declarations: [AppComponent, MonthComponent, DayComponent, HeaderComponent],
+  declarations: [AppComponent, MonthComponent, DayComponent, HeaderComponent, IconButtonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
